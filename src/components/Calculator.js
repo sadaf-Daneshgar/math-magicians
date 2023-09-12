4e4a23aaa752fe8a366c55e029f963350795cbb8
“sadaf <sadaf.batekzai54321@gmial.com>
2023-09-12 16:20:18 +0430

    Added the logic folder and into that add the calculate.js and operate.js file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,77 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import InputNumber from './Input';
+import calculate from '../logic/calculate';
 
-function Calculator() {
+const Calculator = () => {
+  const [numbers, setNumbers] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const ClickEventHandlers = (event) => {
+    setNumbers(calculate(numbers, event));
+  };
   return (
     <>
       <div className="container">
-        <InputNumber className="numInput" value="0" />
+        <InputNumber className="numInput" value={numbers.next || numbers.total || '0'} />
         <div className="rows-contanier">
           <div className="horizontal-rows">
-            <button className="num-buttons" type="button">
+            <button className="num-buttons" type="button" onClick={() => ClickEventHandlers('AC')}>
               AC
             </button>
-            <button className="num-buttons" type="button">
+            <button className="num-buttons" type="button" onClick={() => ClickEventHandlers('+/-')}>
               +/-
             </button>
-            <button className="num-buttons" type="button">
+            <button className="num-buttons" type="button" onClick={() => ClickEventHandlers('%')}>
               %
             </button>
-            <button className="sign-btn" type="button">
+            <button className="sign-btn" type="button" onClick={() => ClickEventHandlers('+')}>
               +
             </button>
           </div>
           <div className="horizontal-rows">
-            <button className="num-buttons" type="button">
+            <button className="num-buttons" type="button" onClick={() => ClickEventHandlers('7')}>
               7
             </button>
-            <button className="num-buttons" type="button">
+            <button className="num-buttons" type="button" onClick={() => ClickEventHandlers('8')}>
               8
             </button>
-            <button className="num-buttons" type="button">
+            <button className="num-buttons" type="button" onClick={() => ClickEventHandlers('9')}>
               9
             </button>
-            <button className="sign-btn" type="button">
-              *
+            <button className="sign-btn" type="button" onClick={() => ClickEventHandlers('x')}>
+              x
             </button>
           </div>
           <div className="horizontal-rows">
-            <button className="num-buttons" type="button">
+            <button className="num-buttons" type="button" onClick={() => ClickEventHandlers('4')}>
               4
             </button>
-            <button className="num-buttons" type="button">
+            <button className="num-buttons" type="button" onClick={() => ClickEventHandlers('5')}>
               5
             </button>
-            <button className="num-buttons" type="button">
+            <button className="num-buttons" type="button" onClick={() => ClickEventHandlers('6')}>
               6
             </button>
-            <button className="sign-btn" type="button">
+            <button className="sign-btn" type="button" onClick={() => ClickEventHandlers('-')}>
               -
             </button>
           </div>
           <div className="horizontal-rows">
-            <button className="num-buttons" type="button">
+            <button className="num-buttons" type="button" onClick={() => ClickEventHandlers('1')}>
               1
             </button>
-            <button className="num-buttons" type="button">
+            <button className="num-buttons" type="button" onClick={() => ClickEventHandlers('2')}>
               2
             </button>
-            <button className="num-buttons" type="button">
+            <button className="num-buttons" type="button" onClick={() => ClickEventHandlers('3')}>
               3
             </button>
-            <button className="sign-btn" type="button">
+            <button className="sign-btn" type="button" onClick={() => ClickEventHandlers('+')}>
               +
             </button>
           </div>
           <div className="horizontal-rows">
-            <button className="num-zero" type="button">
+            <button className="num-zero" type="button" onClick={() => ClickEventHandlers('0')}>
               0
             </button>
-            <button className="num-buttons" type="button">
+            <button className="num-buttons" type="button" onClick={() => ClickEventHandlers('.')}>
               .
             </button>
-            <button className="sign-btn" type="button">
+            <button className="sign-btn" type="button" onClick={() => ClickEventHandlers('=')}>
               =
             </button>
           </div>
@@ -79,6 +89,6 @@ function Calculator() {
       </div>
     </>
   );
-}
+};
 
 export default Calculator;
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function InputNumber({ className, value }) {
-  return <div className={className}>{value}</div>;
-}
+const InputNumber = ({ className, value }) => (
+  <div className={className}>{value}</div>
+);
 
 InputNumber.propTypes = {
   className: PropTypes.string.isRequired,
